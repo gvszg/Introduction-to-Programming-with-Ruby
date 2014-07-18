c0317f69122ce5f15ca83d6ff3295d554ab9bff7
gvszg <zetaentranceserious@gmail.com>
2014-07-18 18:43:06 +0800

    variables done

---
+++ variables/name.rb
@@ -0,0 +1,24 @@
+#puts "Hi,what's your name?"
+
+#puts name = gets.chomp
+
+#puts "How are you, " + name + "!"
+
+
+
+
+#10.times do
+#	puts name
+#end
+
+puts "what's your first name?"
+
+puts first = gets.chomp
+
+puts "And your last name,please?"
+
+puts last = gets.chomp
+
+puts "Welcome, " + first + " " + last + " !"
+
+
---
+++ variables/test.rb
@@ -0,0 +1,14 @@
+
+
+
+
+y = 0
+
+3.times do 
+	y+= 1
+
+	x = y
+
+end
+
+puts x
